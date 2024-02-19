a24fe85569fdfb17e5ccac2f48cb52c5c3e6c031
toxtaboyevsamandar72 <toxtaboyevsamandar720@gmail.com>
2024-02-19 21:44:05 +0500

    index html

---
+++ js/script.js
@@ -0,0 +1,111 @@
+  //  1 console
+
+  // alert('hello')
+  // console.log("samandar");
+
+// let ism = 'samandar';
+// let yosh = '19';
+
+// console.log(ism, yosh);
+
+// yosh = 20;
+// console.log(yosh);
+
+// const familya = "To'xtaboyev"
+// console.log(familya);
+
+// var joy = "farg'ona"
+// joy = "farg'ona tumani"
+
+// console.log(joy);
+
+// string 
+
+// console.log('hello world');
+
+// let email = 'samndartoxtaboyev@gmail.com'
+// console.log(email);
+
+// // string concatenation
+
+
+// let firstName = 'samandar'
+// let lastName = "to'xtaboyev"
+// let fullName = firstName + ' ' + lastName
+
+// console.log(fullName);
+
+// // getting characters
+
+// console.log(fullName[2]);
+
+// // string length => uzunlik 
+
+// console.log(fullName.length);
+
+// // string methods => funksiyaga ohshash ish bajaradi
+
+// console.log(fullName.toUpperCase());
+
+// let res = fullName.toLowerCase()
+// console.log(res);
+
+// let index = email.indexOf('@')
+
+// console.log(index);
+
+// let email = 'samandartoxtaboyev@gmail.com'
+
+
+
+
+// lastInde0f
+
+// let res = email.lastIndexOf('a')
+// console.log(res);
+
+// slice
+
+// let res = email.slice(4, 10)
+// console.log(res);
+
+// let res = email.substr(4, 10).length
+// console.log(res);
+
+// let res = email.replace('a', 'd')
+// console.log(res);
+
+// let res = email.charAt()
+// console.log(res);
+
+// let res = email.trim()
+// console.log(res);
+
+// let res = email.split('a')
+// console.log(res);
+
+
+// let radius = 10 
+// const pi = 3.14
+
+// console.log(radius, pi);
+
+
+
+// console.log(true, false,  'true', 'false');
+
+// let email = 'samandartoxtaboyev@gmail.com'
+// let res  = email.includes('!')
+// console.log(res);
+
+
+// let age = 24
+
+// console.log(age != 25);
+
+// let natija = '100'
+
+// console.log(natija+1);
+
+// let res = Number(1615)
+// console.log(res);
